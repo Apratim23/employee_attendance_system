0bb1306bd01ead485e9ca3cec2e8424438b860e8
Apratim23 <apratimdutta786687@gmail.com>
2025-06-11 21:51:55 +0530

    Added homepage

--- employee_project/employee_project/urls.py
+++ employee_project/employee_project/urls.py
@@ -24,6 +24,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from employees.views import charts_view
+from employees.views import home_view
+
 
 
 router = routers.DefaultRouter()
@@ -49,6 +51,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # Token auth endpoint
     path('dashboard/', charts_view, name='charts-dashboard'),
+    path('', home_view, name='home'),
 ]
 urlpatterns += [
     path('api-token-auth/', obtain_auth_token),
