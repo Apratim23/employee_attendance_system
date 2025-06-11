1e9ad819beff31a98c9382c5b5d9f2ff93e7f31c
Apratim23 <apratimdutta786687@gmail.com>
2025-06-11 22:00:15 +0530

    Added a few changes

--- employee_project/employees/views.py
+++ employee_project/employees/views.py
@@ -4,6 +4,11 @@ from .models import Employee, Department
 from .serializers import EmployeeSerializer, DepartmentSerializer
 from rest_framework.filters import OrderingFilter
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.http import JsonResponse
+from django.views import View
+from rest_framework.authtoken.views import ObtainAuthToken
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
@@ -23,3 +28,11 @@ def charts_view(request):
 
 def home_view(request):
     return render(request, 'home.html')
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CustomTokenAuthView(ObtainAuthToken):
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({
+            "info": "Send POST with username & password to get token.",
+            "example": {"username": "admin", "password": "admin123"}
+        })
